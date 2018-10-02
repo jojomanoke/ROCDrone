3fb2018aa3224de655fc254fdf2c1f4a3abd28c6
pjongman <pascaljongman@hotmail.nl>
2018-10-02 11:56:17 +0200

    final product Login screen

--- app/src/main/java/nl/friesepoort/rocdrone/MainActivity.java
+++ app/src/main/java/nl/friesepoort/rocdrone/MainActivity.java
@@ -1,7 +1,11 @@
 package nl.friesepoort.rocdrone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button yourButton = (Button) findViewById(R.id.dummy);
+
+        yourButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, home.class));
+            }
+        });
     }
 }
+
+
