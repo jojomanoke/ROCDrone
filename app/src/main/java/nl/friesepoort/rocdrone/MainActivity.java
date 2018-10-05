7020ed122bcd936c1c8b548eb51345f44d14a79b
Wagenpark RocIT <rick.smallenbroek@gmail.com>
2018-10-05 12:01:34 +0200

    Login v0.0000000000000000000000000000000000000019

--- app/src/main/java/nl/friesepoort/rocdrone/MainActivity.java
+++ app/src/main/java/nl/friesepoort/rocdrone/MainActivity.java
@@ -6,22 +6,66 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 3;
+
+    //Info.setText("Number of attempts remaining: 3");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button yourButton = (Button) findViewById(R.id.button);
+        Button yourButton = (Button)findViewById(R.id.buttonlogin);
 
         yourButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, home.class));
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
             }
         });
+
+            }
+        });
+
+        Name = (EditText)findViewById(R.id.email);
+        Password = (EditText)findViewById(R.id.password);
+        Info = (TextView)findViewById(R.id.lainfo);
+        Login = (Button)findViewById(R.id.buttonlogin);
+
+
     }
+
+
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(MainActivity.this , home.class);
+            startActivity(intent);
+        }else{
+            counter--;
+                Info.setText("Number of attempts remaining: " + String.valueOf(counter));
+            if(counter == 0){
+                Login.setEnabled(false);
+            }
+        }
+
+    }
+
+
+
 }
 
 
