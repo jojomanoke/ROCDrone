6c30f6d51095107afd1247fd44b092040848b80b
multidutch <pascaljongman@hotmail.nl>
2018-10-17 13:26:22 +0200

    mainactivity login aangepast naar welkomscherm

--- app/src/main/java/nl/friesepoort/rocdrone/MainActivity.java
+++ app/src/main/java/nl/friesepoort/rocdrone/MainActivity.java
@@ -13,78 +13,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText Name;
-    private EditText Password;
-    private TextView Info;
-    private Button Login;
-    private int counter = 3;
-
-    //Info.setText("Number of attempts remaining: 3");
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button yourButton = (Button)findViewById(R.id.buttonlogin);
+        Button yourButton = (Button) findViewById(R.id.button);
 
-        yourButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-
-
-
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
+        yourButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                startActivity(new Intent(MainActivity.this, home.class));
             }
         });
 
-            }
-        });
-
-        Name = (EditText)findViewById(R.id.email);
-        Password = (EditText)findViewById(R.id.password);
-        Info = (TextView)findViewById(R.id.lainfo);
-        Login = (Button)findViewById(R.id.buttonlogin);
-
-
     }
-
-
-// login function
-    private void validate(String userName, String userPassword){
-        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
-            Intent intent = new Intent(MainActivity.this , home.class);
-            startActivity(intent);
-        }else{
-            Toast toast = Toast.makeText(this, "Wrong username or password", Toast.LENGTH_SHORT  );
-            toast.show();
-            counter--;
-                Info.setText("Number of attempts remaining: " + String.valueOf(counter));
-            if(counter == 0){
-                alert();
-                Login.setEnabled(false);
-            }
-        }
-
-    }
-    public void alert(){
-        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
-        myAlert.setMessage("Wrong username or password used to many times.")
-            .setPositiveButton("ok", new DialogInterface.OnClickListener(){
-                @Override
-            public void onClick(DialogInterface dialog, int which){
-                    dialog.dismiss();
-                }
-        } )
-                .create();
-            myAlert.show();
-
-    }
-
-
-
 }
 
 
